bf3eadbdec63857e32180d543b90e270f14051e1
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-23 03:46:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,11 +6,12 @@ def join_nested_strings(src)
   while row_index < src.length do
     element_index = 0
     while element_index < src[row_index].length do
-      if src[row_index][element_index]string_phrase += 
-        
+      if src[row_index][element_index].is_a? String
+        string_phrase += src[row_index][element_index]
       end
       element_index += 1
     end
+    row_index += 1
   end
-  
+  string_phrase
 end
